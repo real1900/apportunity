d88676be449d49f7a7f7a4613f1b1771fe57d416
real1900 <suleman361@gmail.com>
2022-10-15 00:04:49 -0700

    updatedd

--- src/components/Projects/SingleProject/SingleProject.js
+++ src/components/Projects/SingleProject/SingleProject.js
@@ -14,30 +14,30 @@ function SingleProject({
   theme,
   onClick,
 }) {
-  const useStyles = makeStyles((t) => ({
-    iconBtn: {
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      width: 40,
-      height: 40,
-      borderRadius: 50,
-      border: `2px solid ${theme.tertiary}`,
-      color: theme.tertiary,
-      transition: "all 0.2s",
-      "&:hover": {
-        backgroundColor: theme.secondary,
-        color: theme.primary,
-        transform: "scale(1.1)",
-        border: `2px solid ${theme.secondary}`,
-      },
-    },
-    icon: {
-      fontSize: "1.1rem",
-      transition: "all 0.2s",
-      "&:hover": {},
-    },
-  }));
+  // const useStyles = makeStyles((t) => ({
+  //   iconBtn: {
+  //     display: "flex",
+  //     alignItems: "center",
+  //     justifyContent: "center",
+  //     width: 40,
+  //     height: 40,
+  //     borderRadius: 50,
+  //     border: `2px solid ${theme.tertiary}`,
+  //     color: theme.tertiary,
+  //     transition: "all 0.2s",
+  //     "&:hover": {
+  //       backgroundColor: theme.secondary,
+  //       color: theme.primary,
+  //       transform: "scale(1.1)",
+  //       border: `2px solid ${theme.secondary}`,
+  //     },
+  //   },
+  //   icon: {
+  //     fontSize: "1.1rem",
+  //     transition: "all 0.2s",
+  //     "&:hover": {},
+  //   },
+  // }));
 
   // const classes = useStyles();
 
