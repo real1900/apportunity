7e78ddac2eff8f54da4e5e529ddf317bd25c8e97
Suleman Imdad - vn5470y <Suleman.Imdad@walmart.com>
2022-10-17 02:00:25 -0700

    updated

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -6,19 +6,27 @@ import './Experience.css';
 
 import { experienceData } from '../../data/experienceData'
 import ExperienceCard from './ExperienceCard';
+import expimg from '../../assets/png/experience/expertise.png'
 
 function Experience() {
 
     const { theme } = useContext(ThemeContext);
     return (
-        <div className="experience" id="experience" style={{backgroundColor: theme.secondary}}> 
-             <div className="experience-body">
-                 <div className="experience-image">
-                     <img src={theme.expimg} alt="" />
-                 </div>
-                 <div className="experience-description">
-                    <h1 style={{color:theme.primary}}>Expertise</h1>
-                    {experienceData.map(exp =>(
+
+        <div className="experience" id="experience" style={{backgroundColor: theme.secondary}}>
+        <div className="line-styling">
+          <div className="style-circle" style={{backgroundColor: theme.primary}}></div>
+          <div className="style-circle" style={{backgroundColor: theme.primary}}></div>
+          <div className="style-line" style={{backgroundColor: theme.primary}}></div>
+          
+        </div>
+       
+        <div className="experience-body">
+       
+            <div className="experience-description">
+            <h2 style={{color: theme.primary}}>{experienceData.title}</h2>
+                <p style={{color:theme.tertiary80}}>{experienceData.description}</p><br/><br/>
+                {experienceData.types.map(exp =>(
                         <ExperienceCard 
                             key={exp.id}
                             id={exp.id}
@@ -26,9 +34,40 @@ function Experience() {
                             icon = {exp.icon}
                             company={exp.company}/>
                     ))}
-                 </div>
-             </div>
+            </div>
+            <div className="experience-image">
+                <img 
+                    src={expimg}  
+                    alt="Suleman Imdad" 
+                />
+            </div>
+            
         </div>
+    
+    </div>
+
+        // <div className="experience" id="experience" style={{backgroundColor: theme.secondary}}> 
+        //      <div className="experience-body">
+                
+        //          <div className="experience-description">
+        //             <h1 style={{color:theme.primary}}>Expertise</h1>
+        //             <div className="experience-details">
+        //             <h4 style={{color:theme.tertiary80}}>we have experience</h4>
+        //             <div className="experience-image">
+        //              <img src={expimg} alt="" />
+        //          </div>
+        //             <br/><br/> </div>
+        //             {experienceData.map(exp =>(
+        //                 <ExperienceCard 
+        //                     key={exp.id}
+        //                     id={exp.id}
+        //                     jobtitle={exp.jobtitle}
+        //                     icon = {exp.icon}
+        //                     company={exp.company}/>
+        //             ))}
+        //          </div>
+        //      </div>
+        // </div>
     )
 }
 
--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -3,8 +3,11 @@ import android from '../assets/svg/experience/android.svg';
 import flutter from '../assets/svg/experience/flutter.svg';
 import reactNative from '../assets/svg/experience/reactNative.svg';
 
-export const experienceData = [
-    {
+export const experienceData = {
+    title : "Expertise",
+    description:  "We specialize in building all major mobile platforms for native iOS and Android , as well as cross-platform experiences using React, React Native and Flutter. ",
+    types : 
+    [{
         id: 1,
         company: 'Native iOS Development',
         jobtitle: 'Objective-C , Swift',
@@ -27,5 +30,5 @@ export const experienceData = [
         company: 'Cross Platform iOS & Android Development',
         jobtitle: 'React Native',
         icon: reactNative
-    },
-]
+    }]
+}
