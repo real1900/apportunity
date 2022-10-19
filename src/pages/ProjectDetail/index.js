9f4609687a8b68c71201f765dcb6ca3fa2d1a43d
Suleman Imdad - vn5470y <Suleman.Imdad@walmart.com>
2022-10-19 08:11:51 -0700

    Update index.js

--- src/pages/ProjectDetail/index.js
+++ src/pages/ProjectDetail/index.js
@@ -1,4 +1,4 @@
-import React, { useContext , useState, useEffect } from "react";
+import React, { useContext } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { useParams } from "react-router";
 import "./projectDetail.css";
