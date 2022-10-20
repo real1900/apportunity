bdfc4eb340b148d5e98c7f9bb47290a6488a7ec2
Suleman Imdad <suleman.imdad@me.com>
2022-10-20 04:21:36 -0700

    Update projectBody.js

--- src/pages/ProjectDetail/projectBody.js
+++ src/pages/ProjectDetail/projectBody.js
@@ -2,15 +2,14 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
 import './projectBody.css';
 import Markdown from 'markdown-to-jsx';
-
 function ProjectBody() {
  
 const { id } = useParams();
-
 const [markdown, setMarkdown] = useState("");
 
 useEffect(() => {
-  const markDown = require(`../../data/markDown/${id}.md`);
+  // const markDown = require(`../../data/markDown/${id}.md`);
+   const markDown = require("../../data/markDown/1.md");
 
   fetch(markDown)
     .then((res) => res.text())
@@ -20,7 +19,7 @@ useEffect(() => {
 
 
   return (
-    <div className="markdown" >
+    <div className="markdown"  >
     <Markdown>{markdown}</Markdown>;
     </div>
   );
