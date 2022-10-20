a8119af0384761ecf442b38b7f4cc653231e78ec
Suleman Imdad <suleman.imdad@me.com>
2022-10-20 04:46:47 -0700

    updated

--- src/pages/ProjectDetail/projectBody.js
+++ src/pages/ProjectDetail/projectBody.js
@@ -1,11 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect , useContext} from "react";
 import { useParams } from "react-router";
 import './projectBody.css';
 import Markdown from 'markdown-to-jsx';
+
+import { ThemeContext } from "../../contexts/ThemeContext";
+
 function ProjectBody() {
  
 const { id } = useParams();
 const [markdown, setMarkdown] = useState("");
+const { theme } = useContext(ThemeContext);
 
 useEffect(() => {
   // const markDown = require(`../../data/markDown/${id}.md`);
@@ -20,6 +24,11 @@ useEffect(() => {
 
   return (
     <div className="markdown"  >
+       <div className="line-styling">
+              <div className="style-circle" style={{backgroundColor: theme.primary}}></div>
+              <div className="style-circle" style={{backgroundColor: theme.primary}}></div>
+              <div className="style-line" style={{backgroundColor: theme.primary}}></div>
+            </div>
     <Markdown>{markdown}</Markdown>;
     </div>
   );
