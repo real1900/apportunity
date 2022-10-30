aa6c0151ea92898c4f58913310b2b1666254110a
Suleman Imdad <suleman.imdad@me.com>
2022-10-30 06:00:45 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -8,12 +8,11 @@ import { Main, BlogPage, ProjectPage, ProjectDetail } from "./pages";
 import { BackToTop } from "./components";
 import ScrollToTop from "./utils/ScrollToTop";
 import { AnimatePresence } from 'framer-motion';
-import "./App.css";
 import { EventEmitter } from "./utils/events";
 import BlogDetail from "./pages/Blog/BlogDetail";
 import PrivacyPolicy from "./pages/Privacy";
-import MessengerCustomerChat from 'react-messenger-customer-chat';
-
+import Messenger from "./components/Messenger";
+import "./App.css";
 
 function AnimatedSwitch() {
   const [animationClassNames, setAnimationClassNames] = useState("");
@@ -45,10 +44,7 @@ function App() {
         <ScrollToTop />
         <AnimatedSwitch />
       </Router>
-      <MessengerCustomerChat
-        pageId="118398932896752"
-        appId="872759943882027"
-      />
+      <Messenger/>
       <BackToTop />
     </div>
   );
---
+++ src/components/Messenger/index.js
@@ -0,0 +1,16 @@
+import React, { useContext } from "react";
+import MessengerCustomerChat from "react-messenger-customer-chat/lib/MessengerCustomerChat";
+import { ThemeContext } from "../../contexts/ThemeContext";
+
+function Messenger() {
+    const { theme } = useContext(ThemeContext);
+    return (
+        <MessengerCustomerChat
+                themeColor={theme.primary}
+                pageId="118398932896752"
+                appId="872759943882027"
+            />
+    );
+}
+
+export default Messenger;
