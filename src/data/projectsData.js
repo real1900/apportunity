fb0507a139b90eb59c39634ec09a4d0d28376897
Suleman Imdad <suleman.imdad@me.com>
2022-11-24 01:28:41 -0800

    updated

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -11,7 +11,7 @@ export const projectsData = [
     },
     {
         id: 2,
-        projectName: 'DirecTV iPad App',
+        projectName: 'DirecTV iPad',
         companyName: 'DirecTV Inc.',
         projectDesc: 'An extremely app that lets users watch videos on iPad, stream liveTV, set DVR remotely, turn iPad into a remote control, and more',
 
@@ -76,5 +76,38 @@ export const projectsData = [
         companyName: 'Apple Inc.',
         projectDesc: "An iOS and Apple Watch app created by us for Apple Inc. for Apple IT Staff to record health hazards across Apple manufacturing facilities using just the Apple Watch and transfer to Apple's incident management system",
         enterpriseApp: true,
+    },
+    {
+        id: 10,
+        projectName: 'Stream TV Mobile',
+        companyName: 'Verizon',
+        projectDesc: "This app lets you download, stream, buy or rent thousands of movies and TV shows, and watch anytime on your mobile device.",
+        enterpriseApp: false,
+        ios: "https://apps.apple.com/us/app/stream-tv-mobile/id1535456434",
+    },
+    {
+        id: 11,
+        projectName: 'Amazon Alexa',
+        companyName: 'Amazon',
+        projectDesc: "We assisted Amazon in building support for Amazon frames with Amazon Alexa app. This app was built using React Native to manage Alexa-enabled Amazon echo frames, train and talk to Alexa using Amazon Glasses with wireless bone conducting speakers.",
+        enterpriseApp: false,
+        ios: "https://apps.apple.com/us/app/amazon-alexa/id944011620",
+        android: "https://play.google.com/store/apps/details?id=com.amazon.dee.app",
+    },
+    {
+        id: 12,
+        projectName: 'Nike',
+        companyName: 'Nike',
+        projectDesc: "Nike app provides its members with a world class shopping experience. We helped Nike streamline the signup and new user onboarding flows to provide a better shopping experience for its members.",
+        enterpriseApp: false,
+        ios: "https://apps.apple.com/us/app/nike-shoes-apparel-stories/id1095459556",
+    },
+    {
+        id: 13,
+        projectName: 'FuelPak FP4',
+        companyName: 'Vance & Hines',
+        projectDesc: "We created this app for Vance & Hines, which makes your iPhone one of the most powerful flash tuners available. It allows users to  select the best exhaust system for your bike for optimized performance and view live sensor data essentials, and more using BLE frame work. ",
+        enterpriseApp: false,
+        ios: "https://apps.apple.com/tt/app/fuelpak-fp4/id1513978426",
     },
 ]
