de1c44bce87f9e11ca9e442e6b5618a5823320cb
Suleman Imdad <suleman.imdad@me.com>
2022-11-29 01:02:43 -0800

    Update testimonialsData.js

--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -11,42 +11,42 @@ import boy3 from '../assets/svg/testimonials/boy3.svg'
 export const testimonialsData = [
     {
         id: 1,
-        name: 'Chandler Bing',
+        name: 'Nicholas Gordon',
         title: 'Chief Architech',
-        text: "My journey of finding the right iOS development partner finally came to an end with Apportunity. In one word I will call them stalwart in their industry. They mapped my project into beautiful wireframe and finally delivered the right app to complement my business.",
+        text: "My journey of finding the right iOS development partner finally came to an end with Apportunity. In one word I will call them stalwart in their industry.",
         image: boy2
     },
     {
         id: 2,
-        name: 'Monica Geller',
+        name: 'Nellie Robles',
         title: 'CTO',
-        text: "We had a great experience with Apportunity. They are reliable and efficient. They met project deliverable dates and worked very collaboratively with members of our team. We look forward to continue to work with them in the future.",
+        text: "We had a great experience with Apportunity. They met project deliverable dates and worked very collaboratively with members of our team.",
         image: girl2
     },
     {
         id: 3,
-        name: 'Phoebe Buffay',
+        name: 'Tamara Hall',
         title: 'Director of Engineering',
         text: "I couldn’t be happier with my app as well as the quick service in updating the app when iOS updates went out. I highly recommend Apportunity",
         image: girl1
     },
     {
         id: 4,
-        name: 'Joey Tribbiani',
+        name: 'Millard Lacasse',
         title: 'Manager',
         text: "Apportunity has done a Great job in creating and maintaining our App. We would highly recommend them",
         image: boy1
     },
     {
         id: 5,
-        name: 'Rachel Green',
+        name: 'Jackie Miller',
         title: 'CEO',
         text: 'The development team is wonderful, and great to work with. I’m very glad that we went with Apportunity and am very satisfied with the finished product.',
         image: girl3
     },
     {
         id: 6,
-        name: 'Ross Geller',
+        name: 'Darrell Jeffries',
         title: 'President',
         text: "We have had an incredible experience; timelines actually being met, creative collaborations. Apportunity has exceeded our expectations and showed us what working with professionals was like.",
         image: boy3
